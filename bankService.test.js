48888266b9152f52b93e068c6a041f5c48ea997e
Florine <florine.cercle+perso@gmail.com>
2022-02-26 15:44:14 +0100

    create test: print statement in an empty account

--- bankService.test.js
+++ bankService.test.js
@@ -1,9 +1,13 @@
-const { deposit, withdrawal } = require('./bankService');
+const { deposit, withdrawal, printStatements } = require('./bankService');
 
-test('when making a deposit of 10€ on an empty account the balance equals 10€', () => {
+test('In a bank account without operations, the history of operations is empty', () => {
+  expect(printStatements()).toStrictEqual([]);
+});
+
+test('when doing a deposit of 10€ on an empty account the balance equals 10€', () => {
   expect(deposit(10)).toBe(10);
 });
 
-test('when making a withdrawal of 10€ on an account with 10€ balance, the balance equals 0€', () => {
+test('when doing a withdrawal of 10€ on an account with 10€ balance, the balance equals 0€', () => {
   expect(withdrawal(10)).toBe(0);
 });
