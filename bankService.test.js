8dffa210eb528791fd60ca95ff90b30172c8e822
Florine <florine.cercle+perso@gmail.com>
2022-02-26 15:23:29 +0100

    create test: withdrawal 10€ on an account with a 10€ balance

--- bankService.test.js
+++ bankService.test.js
@@ -1,4 +1,9 @@
-const { deposit } = require('./bankService');
+const { deposit, withdrawal } = require('./bankService');
+
 test('when making a deposit of 10€ on an empty account the balance equals 10€', () => {
   expect(deposit(10)).toBe(10);
+});
+
+test('when making a withdrawal of 10€ on an account with 10€ balance, the balance equals 0€', () => {
+  expect(withdrawal(10)).toBe(0);
 });
