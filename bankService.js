6c72a0c5d1a31714bcf7c909e455d61f5a5bdada
Florine <florine.cercle+perso@gmail.com>
2022-02-26 15:26:44 +0100

    validate test: withdrawal of 10€ on an account with 10€ balance

--- bankService.js
+++ bankService.js
@@ -1,6 +1,13 @@
 let balance = 0;
+
 function deposit(amount) {
   balance += amount;
   return balance;
 }
-module.exports = { deposit };
+
+function withdrawal(amount) {
+  balance -= amount;
+  return balance;
+}
+
+module.exports = { deposit, withdrawal };
