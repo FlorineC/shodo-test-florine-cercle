adfe747d5c69b069ad00646785151baf56539892
Florine <florine.cercle+perso@gmail.com>
2022-02-26 18:59:25 +0100

    refactor printing statements service according to the tests previously changed (usage of two columns debit and credits instead of unique amount column)

--- bankService.js
+++ bankService.js
@@ -15,33 +15,34 @@ class StatementHistory {
 }
 
 class Statement {
-  constructor(date, amount) {
+  constructor(date, credit, debit) {
     this.date = date;
-    this.amount = amount;
+    this.credit = credit;
+    this.debit = debit;
   }
 }
 
 let balance = 0;
 const statementHistory = new StatementHistory();
 
-function doOperation(amount) {
-  balance += amount;
-  createStatementInHistory(amount);
+function doOperation(credit, debit) {
+  balance = balance + credit - debit;
+  createStatementInHistory(credit, debit);
   return balance;
 }
 
-function createStatementInHistory(amount) {
+function createStatementInHistory(credit, debit) {
   const formattedDate = formatDate(new Date());
-  const statement = new Statement(formattedDate, amount);
+  const statement = new Statement(formattedDate, credit, debit);
   statementHistory.addStatement(statement);
 }
 
 function deposit(amount) {
-  return doOperation(+amount);
+  return doOperation(amount, 0);
 }
 
 function withdrawal(amount) {
-  return doOperation(-amount);
+  return doOperation(0, amount);
 }
 
 function printStatements() {
